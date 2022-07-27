0112e71484e9a124e4eaa5025a0e70b0c8237019
marlowe <marlowepdx@gmail.com>
2022-07-26 19:14:31 -0700

    seeds and schema created

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (name)
+VALUES 
+('IT'),
+('HR'),
+('Sales'),
+('Accounting');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Full Stack Developer', 80000, 1),
+('Software Engineer', 120000, 2),
+('Accountant', 10000, 3), 
+('Data Analyst', 150000, 4t),
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Maryus', 'Suyram', 2, null),
+('Angela', 'Alegna', 1, 1),
+('Mac', 'Cam', 4, null),
+('Marlowe', 'Ewolram', 3, 3),
+('Lillie', 'Eillil', 6, null),
+('Kayla', 'Alyak', 5, 5),
+('Lewis', 'Allen', 7, null),
+('Katherine', 'Green', 8, 7);
+
